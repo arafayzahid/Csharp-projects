391a019a7012daf8f61c2e8ee65f410c14e10e2a
arafayzahid <135236795+arafayzahid@users.noreply.github.com>
2023-06-01 19:04:10 +0500

    Add files via upload
    Wrote a code to find the LCM of 2 numbers in programming language C#

---
+++ lcm of 2#.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, x, y, lcm = 0;
+            Console.Write("Enter the First Number : ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the Second Number : ");
+            num2 = int.Parse(Console.ReadLine());
+            x = num1;
+            y = num2;
+            while (num1 != num2)
+            {
+                if (num1 < num2)
+                {
+                    num1 += x;
+                }
+                else
+                {
+                    num2 += y;
+                }
+            }
+            lcm = num2; // LCM is equal to either num1 or num2
+            Console.WriteLine("Least Common Multiple is: " + lcm);
+            Console.ReadKey();
+        }
+    }
+}
