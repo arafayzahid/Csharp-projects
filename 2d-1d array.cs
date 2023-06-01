18839e2eeb7dcdd293ea73627367e73240be7bfd
arafayzahid <135236795+arafayzahid@users.noreply.github.com>
2023-06-01 19:07:15 +0500

    Add files via upload
    Converted a 2d array into 1d array using C#

---
+++ 2d-1d array.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2d Array:");
+
+            int[,] a = new int[3, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+
+            int r = a.GetLength(0);
+            int c = a.GetLength(1);
+
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    Console.Write(a[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Size of 1d Array is :");
+            int[] b = new int[r * c];
+            Console.WriteLine(b.GetLength(0));
+
+            Console.WriteLine();
+
+            Console.WriteLine("2d Array Converted into 1d Array :");
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    Console.WriteLine(a[i, j]);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
