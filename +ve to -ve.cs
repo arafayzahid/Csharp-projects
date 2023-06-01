f4302bf4d9a097d6d9ddb92b913aa7cd3b6a1bc0
arafayzahid <135236795+arafayzahid@users.noreply.github.com>
2023-06-01 19:20:07 +0500

    Add files via upload
    Converting +ve elements into -ve elements in C#

---
+++ +ve to -ve.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+
+            Console.WriteLine("Enter 5 +ve Elements:");
+            for (int i = 0; i < 5; i++)
+            {
+                a[i] = Convert.ToInt16(Console.ReadLine());
+                if (a[i] > 0)
+                    a[i] = -a[i];
+            }
+
+            Console.WriteLine("-ve Elements:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
