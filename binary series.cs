f4a3400106e06096ac1503e758f28e6c4e1e9dab
arafayzahid <135236795+arafayzahid@users.noreply.github.com>
2023-06-01 18:56:24 +0500

    Add files via upload
    Wrote a code using C# language for printing binary series.

---
+++ binary series.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (a == 1)
+                    {
+                        Console.Write("0");
+                        a = 0;
+                    }
+                    else if (a == 0)
+                    {
+                        Console.Write("1");
+                        a = 1;
+                    }
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
