53d8a3b82890121a005f7dd5665c7826a5ef9d6e
arafayzahid <135236795+arafayzahid@users.noreply.github.com>
2023-06-01 18:47:58 +0500

    Add files via upload
    Write a C# code for printing the area of triangle

---
+++ area of triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.Write("Enter The Length Of The Triangle =");
+                double length1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter The Base Of The Triangle =");
+                double base1 = Convert.ToDouble(Console.ReadLine());
+
+                double area = (length1 * base1) / 2;
+                Console.WriteLine("The Area Of A Triangle Is =" + area);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
