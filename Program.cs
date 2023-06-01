507c0144fb6cf3253d2acb2139f15bc187a37b11
arafayzahid <135236795+arafayzahid@users.noreply.github.com>
2023-06-01 18:30:59 +0500

    Add files via upload
    Fibonacci series in C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fab_series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int c = 0;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            for (int i = 0; i <= 8; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(c);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
