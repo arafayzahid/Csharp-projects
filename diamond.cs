3f3c7504f042068fefaf6e7eb66ca66fc1f496a5
arafayzahid <135236795+arafayzahid@users.noreply.github.com>
2023-06-01 18:59:16 +0500

    Add files via upload
    Print a star diamond using C# programming language.

---
+++ diamond.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i += 2)
+            {
+                Console.WriteLine(" ");
+                for (int j = i; j <= 5; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+            }
+
+            for (int i = 3; i >= 1; i -= 2)
+            {
+                Console.WriteLine(" ");
+                for (int j = i; j <= 5; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
