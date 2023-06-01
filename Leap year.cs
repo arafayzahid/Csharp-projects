2ab5b0998e084ce3c294a11fb6253925c100ee2e
arafayzahid <135236795+arafayzahid@users.noreply.github.com>
2023-06-01 18:45:30 +0500

    Add files via upload
    Write a Leap year code using C#

---
+++ Leap year.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int year = 1900; year <= 2030; year++)
+            {
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                {
+                    Console.WriteLine(year + "is a leap year");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
