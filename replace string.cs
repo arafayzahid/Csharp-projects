784099bbeebcc6159c6cf5d3eedacbbc0a9bc928
arafayzahid <135236795+arafayzahid@users.noreply.github.com>
2023-06-01 19:08:27 +0500

    Add files via upload
    Replacing a string using C#

---
+++ replace string.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string s = "Abdul Rafay Zahid";
+            Console.WriteLine("Sentence Before Replacing : {0} ", s);
+
+            string s1 = s.Replace("Rafay", "Haseeb");
+            Console.WriteLine("Sentence After Replacing : {0} ", s1);
+
+            Console.ReadLine();
+        }
+    }
+}
