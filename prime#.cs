9ba112f5ecaf8c64abb3e478e655c44a6f8a1e14
arafayzahid <135236795+arafayzahid@users.noreply.github.com>
2023-06-01 19:25:14 +0500

    Add files via upload
    Prime number using C#

---
+++ prime#.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int r = 0; r <= 100; r++)
+            {
+                int n = r, a = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        a++;
+                    }
+                }
+                if (a == 2)
+                {
+                    Console.WriteLine(n + " Prime Number");
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
