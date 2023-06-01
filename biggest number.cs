d2b950907cabd3a392fcd41a3327ff7e27199d1b
arafayzahid <135236795+arafayzahid@users.noreply.github.com>
2023-06-01 19:22:09 +0500

    Add files via upload
    Finding biggest number using C# code

---
+++ biggest number.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //user input
+            Console.WriteLine("Enter numbers:");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            //condition
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("The biggest number is: " + num1);
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("The biggest number is: " + num2);
+            }
+            else
+            {
+                Console.WriteLine("The biggest number is: " + num3);
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
